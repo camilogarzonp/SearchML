d2a78d4dc4f314064ae2057dd67f080cc2a32db5
Juan Camilo Garzón Patiño <camilogarzon@thepublicgroup.com>
2024-09-03 09:32:30 -0500

    Initial Commit

---
+++ SearchingML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchingML
+//
+//  Created by Juan Camilo Garzon Patino on 3/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchingML/SearchingMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchingMLApp.swift
+//  SearchingML
+//
+//  Created by Juan Camilo Garzon Patino on 3/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchingMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
